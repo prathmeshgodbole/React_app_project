297012477506a9e1b69ced0f320ab9c7320cde8c
prathmeshgodbole <godbole611@gmail.com>
2020-10-13 14:31:37 +0530

    My_App

---
+++ .history/React-js-practice/myapp/src/Ninjas_20201013143024.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import './Ninjas.css'
+const Ninjas = ({ ninjas, deleteNinja }) => {
+    //console.log(this.props);
+    const ninjaList = ninjas.map(ninja => {
+            if (ninja.age > 20) {
+                return ( <
+                    div className = "ninja"
+                    key = { ninja.id } >
+                    <div > Name: { ninja.name } < /div>   <
+                    div > Age: { ninja.age } < /div>  <
+                    div > Belt: { ninja.belt } < /div> <
+                    button onClick = {
+                        () => { deleteNinja(ninja.id) }
+                    } > Delete < /button> < /
+                    div >
+                )
+            } else {
+                return null
+            }
+        })
+        /*const ninjaList=ninjas.map(ninja=>{
+            return ninja.age>20 ?
+            (  <div className="ninja" key={ninja.id}>
+            <div> Name: {ninja.name} </div>  
+            <div> Age: {ninja.age} </div> 
+            <div> Belt: {ninja.belt} </div><br></br>
+           </div>) 
+           : null;
+        })*/
+    return ( <
+        div className = "ninja-List" > { ninjaList } <
+        /div>
+    )
+}
+
+export default Ninjas
